0977de3b530018558dedfd8d825f52c5718572f9
Cody <Cody>
2025-01-29 12:28:59 -0800

    stats

---
+++ stats.py
@@ -0,0 +1,18 @@
+from httprequest import get_profile
+
+# achievements
+# inventory count
+# skills, bonuses, encyclopedia total
+# level, version, playtime
+# side, ragfair rating
+# survivor class
+# quest count (active/complete/total)
+# last played
+# last death
+# victims
+# counters
+# trader info
+# karma
+# hideout
+# insured item count?
+# accuracy somehow??
